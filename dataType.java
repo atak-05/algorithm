e3aa7fc60e8dc908915a43712819979cb9311dc0
gizem <gizemtuncer17@gmail.com>
2023-03-23 13:54:26 +0300

    dataType in Java was worked

--- dataType.java
+++ dataType.java
@@ -1,5 +1,22 @@
+/**
+ * Data Type :
+ * int : tamsayi
+ * String : metinler
+ * double : ondalıklı
+ * boolean : true-false
+ *  
+ * 
+ */
+
 public class dataType {
     public static void main(String[] args) {
-        
+        String name = "Gizem ", lastName = "Çırıkka";
+        int numberOfChildren = 3;
+        double weight = 75.0;
+        boolean isMarried = true;
+
+        System.out.println("Name: " + name +"\n"+ "Last Name: " + lastName + "\n"+ "Number Of Children: " + numberOfChildren + "\n"+ "Weight: \t " + weight + "\n"+ "IsMarried: \t " + isMarried);
+
+
     }
 }
