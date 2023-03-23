ddd234e7bf209e497771d9189e61b420a4f58e5f
gizem <gizemtuncer17@gmail.com>
2023-03-23 13:26:25 +0300

    Variables in Java  was worked

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String[] args) {
+        String name = "Gizem"; 
+        final int id = 5 ; // final variable never change
+        System.out.println("Hello " + name );
+        name = "Cenk";
+        System.out.println("Hello "+ name );
+        System.out.println("id is  product of choice: " + id);
+
+    }
+}
