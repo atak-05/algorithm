08ae2b661689a3b3f7780e2e151711537d5200f2
gizem <gizemtuncer17@gmail.com>
2023-03-24 14:01:21 +0300

    matrix array list create was worked

---
+++ matrixArrays.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class matrixArrays {
+    public static void main(String[] args) {
+        Object [][] complexList = {{1,"Cenk",1.83,true}, {2, "Gizem",null, true} ,{3,"Atak",1.67,null }, {4,"Bıdık"} };
+        
+        for (int i = 0; i<complexList.length; i++) {
+            for (int j = 0; j<complexList[i].length; j++) {
+                System.out.print(complexList[i][j]+ "\t");
+            }
+            System.out.println("");
+        }
+    
+    }
+}
